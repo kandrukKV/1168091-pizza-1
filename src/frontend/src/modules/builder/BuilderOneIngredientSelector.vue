c8d591cfa1fa38d5fd6973f01f79667f3436dbb0
Constantine <constantine@MacBook-Pro-Konstantin.local>
2021-09-19 00:09:27 +0700

    ингредиенты добавляются на пицу

---
+++ src/frontend/src/modules/builder/BuilderOneIngredientSelector.vue
@@ -0,0 +1,30 @@
+<template>
+  <li class="ingredients__item">
+    <span class="filling" :class="`filling--${STUFFING[stuffing.name]}`">
+      {{ stuffing.name }}
+    </span>
+    <RadioButton :count="stuffing.count" />
+  </li>
+</template>
+
+<script>
+import RadioButton from "../../common/components/RadioButton";
+import { STUFFING } from "../../common/constants";
+export default {
+  name: `BuilderStuffingSelector`,
+  components: {
+    RadioButton,
+  },
+  props: {
+    stuffing: {
+      type: Object,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      STUFFING,
+    };
+  },
+};
+</script>
---
+++ src/frontend/src/modules/builder/BuilderSauceSelector.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="ingredients__sauce">
+    <p>Основной соус:</p>
+
+    <label
+      v-for="(sauce, idx) in sauces"
+      :key="sauce.name"
+      class="radio ingredients__input"
+    >
+      <input
+        type="radio"
+        name="sauce"
+        :value="SAUCE[sauce.name]"
+        :checked="idx === 0"
+      />
+      <span>{{ sauce.name }}</span>
+    </label>
+  </div>
+</template>
+
+<script>
+import { SAUCE } from "../../common/constants";
+export default {
+  name: `BuilderSauceSelector`,
+  props: {
+    sauces: {
+      type: Array,
+      required: true,
+    },
+    currentSauce: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      SAUCE,
+    };
+  },
+};
+</script>
