982faa10ae010d90cf7baff31c690a57690053d8
Konstantin Kandruk <kandruk@ya.ru>
2021-09-22 16:10:01 +0700

    начало динамики в хедере

--- src/frontend/src/layouts/AppLayoutDefault.vue
+++ src/frontend/src/layouts/AppLayoutDefault.vue
@@ -1,14 +1,20 @@
 <template>
   <div class="app-layout">
-    <AppLayoutHeader />
+    <AppLayoutHeader :user="user" />
     <slot />
   </div>
 </template>
 
 <script>
 import AppLayoutHeader from "../layouts/AppLayoutHeader";
+import user from "../static/user.json";
 export default {
   name: "AppLayout",
   components: { AppLayoutHeader },
+  data() {
+    return {
+      user,
+    };
+  },
 };
 </script>
--- src/frontend/src/layouts/AppLayoutHeader.vue
+++ src/frontend/src/layouts/AppLayoutHeader.vue
@@ -3,7 +3,7 @@
     <div class="header__logo">
       <a href="#" class="logo">
         <img
-          src="@/assets/img/logo.svg"
+          src="../assets/img/logo.svg"
           alt="V!U!E! Pizza logo"
           width="90"
           height="40"
@@ -13,8 +13,30 @@
     <div class="header__cart">
       <a href="#">0 ₽</a>
     </div>
-    <div class="header__user">
+    <div v-if="user === null" class="header__user">
       <a href="#" class="header__login"><span>Войти</span></a>
+    </div>
+    <div v-else class="header__user">
+      <a href="#">
+        <picture>
+          <source
+            type="image/webp"
+            srcset="
+              ../assets/img/users/user5.webp    1x,
+              ../assets/img/users/user5@2x.webp 2x
+            "
+          />
+          <img
+            src="../assets/img/users/user5.jpg"
+            srcset="../assets/img/users/user5@2x.jpg"
+            :alt="user.name"
+            width="32"
+            height="32"
+          />
+        </picture>
+        <span>{{ user.name }}</span>
+      </a>
+      <a href="#" class="header__logout"><span>Выйти</span></a>
     </div>
   </header>
 </template>
